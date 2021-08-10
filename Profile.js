1d65188a2c35956f746333445622584e1c6f8e93
josephadeabah <joseph.adeabah@amalitech.org>
2021-08-10 12:49:33 +0000

    initial commit

---
+++ Profile.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { fetchUserData, cancelFetch } from './dataFetcher';
+import { Userlist } from './Userlist';
+
+export class Profile extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {userData: null}
+  }
+
+loadUserData(){
+  this.setState({userData: null});
+  this.fetchID = fetchUserData(this.props.username, (userData) => {
+  this.setState({ userData });
+});
+}
+componentDidMount(){
+  this.loadUserData();
+}
+
+componentWillUnmount(){
+  cancelFetch(this.fetchID);
+}
+
+componentDidUpdate(prevProps){
+if(this.props.username !== prevProps.username){
+  cancelFetch(this.fetchID);
+  this.loadUserData();
+}
+}
+
+  render() {
+    let isLoading;
+  isLoading = this.state.userData === null ? true : false
+
+  let name;
+  name = isLoading ? 'Loading...': this.state.userData.name
+  let bio;
+  bio = isLoading ? 'Loading...' : this.state.userData.bio
+
+  let friends;
+  friends = isLoading ? []: this.state.userData.friends
+  let profImage = !isLoading && <img src={this.state.userData.profilePictureUrl} alt="" />
+
+    let className = 'Profile';
+    if (isLoading) {
+      className += ' loading';
+    }
+
+    return (
+      <div className={className}>
+        <div className="profile-picture">
+        {profImage}
+        </div>
+        <div className="profile-body">
+          <h2>{name}</h2>
+          <h3>@{this.props.username}</h3>
+          <p>{bio}</p>
+          <h3>My friends</h3>
+          <Userlist usernames={friends} onChoose={this.props.onChoose} />
+        </div>
+      </div>
+    );
+  }
+}
